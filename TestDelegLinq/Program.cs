1e64e9dc2b88cb6920b0ae9d53e91563aae28b30
Adrian <andrian.melnic2003@gmail.com>
2024-03-18 22:59:28 +0200

    Finished assign

---
+++ TestDelegLinq/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TestDelegLinq;
+
+Console.WriteLine("Hello, World!");
+
+
+List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+Util.Transform(list, Square);
+Console.WriteLine("After transform");
+PrintList(list);
+
+list.DevideByTwo(x => x / 2);
+Console.WriteLine("\nAfter Division");
+PrintList(list);
+
+int closure = 15;
+
+Func<int, int> AddByFactor = delegate (int x)
+{
+    return x + closure;
+};
+closure = 20;
+
+for(int i = 0; i < list.Count; i++ )
+{
+    list[i] = AddByFactor(list[i]);
+}
+Console.WriteLine();
+PrintList(list);
+static int Square(int value) => value * value;
+
+
+static void PrintList(List<int> list)
+{
+    
+    foreach (int item in list)
+    {
+
+        Console.Write(item+ " ");
+    }
+}
---
+++ TestDelegLinq/Util.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDelegLinq
+{
+    public delegate int Transformer(int value);
+    public static class Util
+    {
+        public static void Transform(List<int> list , Transformer t) 
+        {
+            for(int i = 1; i< list.Count; i++)
+            {
+                list[i] = t(list[i]);
+            }
+        }
+
+
+        public static void DevideByTwo( this List<int> list, Func<int, int> func)
+        {
+            for(int i = 1; i< list.Count; i++)
+            {
+                list[i] = func(list[i]);    
+            }
+        }
+    }
+}
